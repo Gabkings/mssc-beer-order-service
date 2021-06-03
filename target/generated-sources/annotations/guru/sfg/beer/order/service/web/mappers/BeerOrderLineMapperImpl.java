de62889e7eb25eed341b2981a13398a21c63d121
Gabriel Gitonga <gabriel.gitonga@bskglobaltech.com>
2021-06-04 02:15:16 +0300

    Adding mysql connection and configuring hikaricp

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderLineMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-06-03T06:21:32+0300",
+    date = "2021-06-04T02:13:17+0300",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 14.0.2 (Oracle Corporation)"
 )
 @Component
